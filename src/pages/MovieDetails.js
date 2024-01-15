73928d54b366f80afa641c6f72d779cb1d70b48a
Shaun Adams <shaunsiadams@gmail.com>
2024-01-15 22:12:49 +0200

    movie

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -22,6 +22,34 @@ function MovieDetails() {
     <div>
       <h2>{movie.title}</h2>
       <p>{movie.overview}</p>
+      <li key={movie.id}>
+            {movie.title},
+            {/* {movie.release_date},
+            {movie.overview},
+            {movie.vote_average},
+            {movie.vote_count},
+            {movie.popularity}, */}
+            {/* {movie.poster_path}, */}
+            {/* {movie.original_language},
+            {movie.original_title},
+            {movie.backdrop_path}, */}
+            {/* {movie.adult},
+            {movie.video},
+            {movie.genre_ids},
+            {movie.media_type}, */}
+            {/* {movie.name}, */}
+            {/* {movie.first_air_date},
+            {movie.origin_country},
+            {movie.original_name},
+            {movie.profile_path},
+            {movie.known_for},
+            {movie.known_for_department}, */}
+            {/* <button onClick={()=>handleClick(movie)}>Details</button> */}
+            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+            <img src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`} alt={movie.title} />
+            {/* <iframe src={`https://video.tmdb.org/t/p/w500${movie.video}`} alt={movie.title} > </iframe> */}
+            {/* <YouTube videoId={movie.video}></YouTube> */}
+          </li>
     </div>
   );
 }
