035d49c90530df41a2f16c72b5eb84cebad5ace7
Shaun Adams <shaunsiadams@gmail.com>
2024-01-15 11:04:52 +0200

    movie list with poster

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { fetchMovies } from '../services/movieService';
+import YouTube from 'react-youtube';
 
 
 function MovieList() {
@@ -11,6 +12,7 @@ function MovieList() {
       try {
         const movieData = await fetchMovies();
         setMovies(movieData);
+        console.log("movie-details " + movieData)
       } catch (err) {
         setError(err.message);
       }
@@ -25,9 +27,38 @@ function MovieList() {
     <div>
       <h1>Popular Movies</h1>
       <ul>
-        {movies.map(movie => (
-          <li key={movie.id}>{movie.title}</li>
-        ))}
+        {movies
+        .map(movie => (
+          <li key={movie.id}>
+            {movie.title},
+            {/* {movie.release_date},
+            {movie.overview},
+            {movie.vote_average},
+            {movie.vote_count},
+            {movie.popularity}, */}
+            {/* {movie.poster_path}, */}
+            {/* {movie.original_language},
+            {movie.original_title},
+            {movie.backdrop_path}, */}
+            {/* {movie.adult},
+            {movie.video},
+            {movie.genre_ids},
+            {movie.media_type}, */}
+            {/* {movie.name}, */}
+            {/* {movie.first_air_date},
+            {movie.origin_country},
+            {movie.original_name},
+            {movie.profile_path},
+            {movie.known_for},
+            {movie.known_for_department}, */}
+            {/* <button onClick={()=>handleClick(movie)}>Details</button> */}
+            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+            <img src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`} alt={movie.title} />
+            {/* <iframe src={`https://video.tmdb.org/t/p/w500${movie.video}`} alt={movie.title} > </iframe> */}
+            {/* <YouTube videoId={movie.video}></YouTube> */}
+          </li>
+        ))
+        }
       </ul>
     </div>
   );
--- src/services/movieService.js
+++ src/services/movieService.js
@@ -5,7 +5,9 @@ const baseUrl = 'https://api.themoviedb.org/3';
 
 export const fetchMovies = async () => {
   try {
-    const response = await axios.get(`${baseUrl}/movie/popular?api_key=${apiKey}`);
+    const response = await axios.get(`${baseUrl}/discover/movie?api_key=${apiKey}`);
+    // fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc', options)
+
     return response.data.results;
   } catch (error) {
     console.error('Error fetching movies:', error);
